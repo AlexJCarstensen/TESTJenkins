354f1a3ae1f7714f43749c58a30dc9be8596de8a
Alex Carstensen <matthacker.alex@gmail.com>
2015-11-06 22:26:17 +0100

    added one test

--- TestJenkins/Coordinate.Unit.Test/CoordinateUnitTest.cs
+++ TestJenkins/Coordinate.Unit.Test/CoordinateUnitTest.cs
@@ -1,7 +1,22 @@
-﻿namespace Coordinate.Unit.Test
+﻿using NUnit.Framework;
+
+namespace Coordinate.Unit.Test
 {
+    [TestFixture]
     public class CoordinateUnitTest
     {
-         
+        private ICoordinateTest _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new CoordinateTest(4,5);
+        }
+
+        [Test]
+        public void GetY_return5()
+        {
+            Assert.That(_uut.y, Is.EqualTo(5));
+        }
     }
 }
